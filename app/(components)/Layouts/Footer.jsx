86da5d12b9c414102e51e966c8f7b51e76a8fe37
Kamran H <kamran.tmu.math@gmail.com>
2023-12-04 23:34:05 +0330

    Add team members

--- app/(components)/Layouts/Footer.jsx
+++ app/(components)/Layouts/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <footer className='flex flex-row font-semibold justify-center bg-gradient-to-l from-gray-300 via-20% to-gray-500 m-1 mt-0'>
-            <h5>All Right Reserved &copy;<a className='hover:cursor-pointer text-red-950'>KMDB</a> <span id='footer-year'>{props.year}</span></h5>
+        <footer
+            className='flex flex-row font-semibold justify-center bg-gradient-to-l from-gray-300 via-20% to-gray-500 m-1 mt-0'>
+            <h5>All Right Reserved &copy;
+                <a className='hover:cursor-pointer text-red-950' title="Members: Kamran, Ghazal, Erfan, Ghambar">KMDB</a>
+                <span id='footer-year'>{props.year}</span>
+            </h5>
         </footer>
     );
 }
