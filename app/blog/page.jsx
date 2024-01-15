2d119a2a54b739bc587895c336c43c6b73a2cd2b
Kamran H <kamran.tmu.math@gmail.com>
2024-01-15 05:55:57 -0500

    Creating & updating blog pages

---
+++ app/blog/page.jsx
@@ -0,0 +1,9 @@
+import Main from "@/app/(components)/Layouts/Main";
+
+export default function Page() {
+    return (
+        <Main>
+            <h1 className="font-bold m-0 p-0">Game Analysis Blog Page</h1>
+        </Main>
+    );
+};
